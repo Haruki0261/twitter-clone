acfe56d0324bfe0ddc180344673dbaba8852bd78
齊藤陽輝 <saitoharuki@HajimariHarukiSaito.local>
2023-08-15 00:11:22 +0900

    :update:ビューにツイート更新の日付を追加

--- twitter/resources/views/tweet/show.blade.php
+++ twitter/resources/views/tweet/show.blade.php
@@ -6,11 +6,16 @@
     <div class="row justify-content-center">
         <div class="col-md-9">
             <div class="card mt-5">
-                <div class="card-body p-3 w-100 d-flex ">
+                <div class="card-header p-3 w-100 d-flex ">
                     <div class="ml-2 d-flex flex-column">
-                        <p class="mb-0 text-wrap">{{ $tweet->user->name }}</p>
-                        <p class="mb-0 text-wrap">{{ $tweet->content }}</p>
+                        <p class="mb-0">{{ $tweet->user->name }}</p>
                     </div>
+                    <div class="d-flex justify-content-end flex-grow-1">
+                        <p class="mb-0">{{ $tweet->updated_at }}</p>
+                    </div>
+                </div>
+                <div class="card-body">
+                    {{ $tweet->content }}
                 </div>
             </div>
         </div>
