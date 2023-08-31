a53a4f380830228748b79d2626eb0209464bef0a
齊藤陽輝 <saitoharuki@HajimariHarukiSaito.local>
2023-08-31 12:29:50 +0900

    いいねの認可

--- twitter/resources/views/top/index.blade.php
+++ twitter/resources/views/top/index.blade.php
@@ -52,7 +52,7 @@
                         <div class="card-footer bg-white">
                             <div class="d-flex justify-content-end">
                                 @if(!$tweet->isFavorite)
-                                    @if($tweet->user_id != Auth::id())
+                                    @if($tweet->author_id != Auth::id())
                                         <form method="POST" action="{{ route('tweet.favorite', ['id' => $tweet->id]) }}"  class="mb-0">
                                             @csrf
                                             <button type="submit" class="btn p-0 border-0"><i class="far fa-heart fa-fw"></i></button>
